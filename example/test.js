b7175a70db30185f23adcc90e897c254020aa8c3
Samuel Reed <samuel.trace.reed@gmail.com>
2016-07-27 15:03:32 -0500

    Refactor:
    Add ESLint
    Export Yub as a reusable object that can be instantiated with multiple keys
    Add retry logic that round-robins through Yubico servers
    Update tests to be fully offline and not require real clientId/secretKey
    Tighten up ambiguities in code
    Add HMAC test addressing #12
    Fix too-long nonce generation

--- example/test.js
+++ example/test.js
@@ -1,16 +1,20 @@
-
+'use strict';
+/* eslint-disable no-console */
 var yub = require('../index.js');
 
 var key = process.argv.length - 1;
 var otp = process.argv[key];
 
+var clientId = process.env.CLIENT_ID || process.env.USER;
+var secretKey = process.env.SECRET_KEY || process.env.PASSWORD;
+
 // initialise the yub library
-yub.init(process.env.USERNAME, process.env.PASSWORD);
+yub.init(clientId, secretKey);
 
 // attempt to verify the key
 yub.verify(otp, function(err,data) {
   if(err) {
-    console.log("Error");
+    console.log('Error');
     process.exit(-1);
   }
   console.log(data);
@@ -18,7 +22,7 @@ yub.verify(otp, function(err,data) {
     console.log(data);
     process.exit(0);
   } else {
-    console.log("Invalid OTP");
+    console.log('Invalid OTP');
     process.exit(-2);
   }
 });
